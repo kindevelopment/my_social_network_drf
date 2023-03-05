e0f988b70ef69855c17b34c10dc14e7fdf96356c
kindevelopment <madeinbosco@inbox.ru>
2023-03-05 11:47:35 +0300

    del settings db

--- social_project/settings.py
+++ social_project/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'new_social',
         'USER': 'kindevelop',
-        'PASSWORD': 'Forward05',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432'
         },
